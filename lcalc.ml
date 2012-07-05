8b2f55d9a9c49ebce119ad6f5d0497ced05ac4fa
Arthur Carcano <arthur.carcano@orange.fr>
2012-07-06 01:05:50 +0200

    added a limitation to the reduction to prevent from infinite loop

--- lcalc.ml
+++ lcalc.ml
@@ -1,5 +1,6 @@
 open Ast
 
+let lim = 10;;
 
 let rec subst e a b =  match e with
 	| Var c as v -> if c=a then b else v
@@ -13,15 +14,15 @@ without, and you won't find (or if you do, please
 email me).
 *)
 
-let rec app2 t = match t with 
-	|App (Abstr (v,e), u) -> application (subst e v u) 
+let rec app2 t n= match t with 
+	|App (Abstr (v,e), u) -> if (n < lim) then application (subst e v u) (n+1) else t
 	| _ -> t
 
-and application t = match t with
-	| App ( Abstr (v,e), u ) -> application (subst e v u)
-	| App ((Var _) as v, t) -> App( v, application t)
-	| App (t1 , t2) -> app2(App (application t1, application t2))
-	| Abstr (c , t) -> Abstr (c , application t)
+and application t n = match t with
+	| App ( Abstr (v,e), u ) -> if (n < lim) then application (subst e v u) (n+1) else t
+	| App ((Var _) as v, t) -> App( v, application t n)
+	| App (t1 , t2) -> app2 (App (application t1 n, application t2 n)) n 
+	| Abstr (c , t) -> Abstr (c , application t n) 
 	| (Var _ )as v ->  v;;
 
-let eval s =  to_string (application (Parser.amorce Lexer.lex (Lexing.from_string s)));;
+let eval s =  to_string (application (Parser.amorce Lexer.lex (Lexing.from_string s)) 0 );;
