7629fc26d000c5979ac595f2c60417d99b1b340a
Arthur Carcano <arthur.carcano@orange.fr>
2012-07-06 01:20:22 +0200

    Error handling

--- http.ml
+++ http.ml
@@ -8,7 +8,7 @@ let service i o =
 	if string_match (regexp "GET .*") s 0 
 		then (let r1 = replace_first (regexp ".*?input=") "" s
 		in let r2 = replace_first (regexp " HTTP/.*") "" r1
-		in let r3 = eval r2
+		in let r3 = try eval r2 with _ -> "Error"
 		in output_string o (r3)); flush o; exit 0
 	done
 	with _ ->();;
